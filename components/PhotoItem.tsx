190fb4347e9e50c318b4ea3094722696597ff732
hehehai <riverhohai@gmail.com>
2022-10-08 20:19:25 +0800

    chore: atlas date

--- components/PhotoItem.tsx
+++ components/PhotoItem.tsx
@@ -2,11 +2,14 @@ import Image from "next/image";
 import { Atlas } from "../pages/interface/photo";
 
 export default function PhotoItem(props: Atlas) {
-  const { title, photos } = props;
+  const { title, photos, time } = props;
 
   return (
     <div>
-      <div className="text-xl mb-4">{title}</div>
+      <div className="text-xl mb-4">
+        <span>{title}</span>
+        <span className="text-sm ml-2">{time}</span>
+      </div>
       <div className="grid grid-cols-3 gap-4">
         {photos.map((photo) => (
           <div className="w-full h-72 relative" key={photo.path}>
