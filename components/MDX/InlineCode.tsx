f4d8e6f4c68ba305ff3d728d0f5c39cd03bcf6eb
hehehai <riverhohai@gmail.com>
2022-11-02 22:17:45 +0800

    fix: article code break style

--- components/MDX/InlineCode.tsx
+++ components/MDX/InlineCode.tsx
@@ -14,7 +14,7 @@ function InlineCode({
   return (
     <code
       className={cn(
-        'inline text-code text-secondary dark:text-secondary-dark bg-gray-200 dark:bg-gray-700 px-1 rounded-md no-underline',
+        'inline text-code text-secondary dark:text-secondary-dark bg-gray-200 dark:bg-gray-700 px-1 rounded-md no-underline break-words',
         {
           'bg-opacity-60 py-px': !isLink,
           'bg-highlight dark:bg-highlight-dark py-0': isLink,
