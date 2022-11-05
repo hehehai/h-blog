3bf182fe4e0b9a5caa5544fd42aca251bb297a05
hehehai <riverhohai@gmail.com>
2022-11-05 11:20:57 +0800

    feat: vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,13 @@ import "../styles/tailwind.css";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class">
       <Component {...pageProps} />
+      <Analytics />
     </ThemeProvider>
   );
 }
