052b319fde01be2942046ee8d9ed555b06aa770d
hehehai <riverhohai@gmail.com>
2022-11-08 22:44:51 +0800

    chore: vercel analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,14 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="zh-cmn-Hans">
       <Head>
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml"/>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <meta
+          httpEquiv="Content-Security-Policy"
+          content="default-src 'self'; script-src vitals.vercel-insights.com"
+        />
       </Head>
       <body className="bg-zinc-50 h-full dark:bg-gradient-to-br dark:from-slate-900 dark:to-black dark:text-slate-50 min-h-screen">
         <Main />
