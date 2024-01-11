ccd1cd7ba83f742af72af15c4fd7a8976aef5df4
hehehai <riverhohai@gmail.com>
2024-01-11 16:56:11 +0800

    chore: site style

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,17 +1,17 @@
 import { cn } from "lib/utils";
-import { Html, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="zh-CN" className={cn("bg-background font-sans antialiased")}>
-      {/* <Head>
+      <Head>
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
-        <link
+        {/* <link
           rel="stylesheet"
           crossOrigin="anonymous"
           href="https://cdn.jsdelivr.net/npm/misans@4.0.0/lib/Normal/MiSans-Regular.min.css"
-        />
-      </Head> */}
+        /> */}
+      </Head>
       <body className="min-h-screen bg-stone-50 text-foreground h-full dark:bg-gradient-to-br dark:from-slate-900 dark:to-background">
         <Main />
         <NextScript />
