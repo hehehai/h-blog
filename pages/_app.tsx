5bbb611696fb658905d0094bf50e88ef05aa6b5f
hehehai <riverhohai@gmail.com>
2024-02-01 14:13:28 +0800

    chore: rm vercel ans

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,11 @@ import "../styles/tailwind.css";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
-import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class">
       <Component {...pageProps} />
-      <Analytics />
     </ThemeProvider>
   );
 }
