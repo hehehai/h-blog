60bd22438eb82c762fdced8da5af58dcedb48c25
hehehai <riverhohai@gmail.com>
2024-02-07 16:50:59 +0800

    chore: with clarity

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,6 +11,13 @@ export default function Document() {
           crossOrigin="anonymous"
           href="https://cdn.jsdelivr.net/npm/misans@4.0.0/lib/Normal/MiSans-Regular.min.css"
         />
+        <script type="text/javascript">
+          {`(function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "kxysqce1d4");`}
+        </script>
       </Head>
       <body className="min-h-screen bg-stone-50 text-foreground h-full dark:bg-gradient-to-br dark:from-slate-900 dark:to-background">
         <Main />
