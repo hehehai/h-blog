403584dee401edd29cdb260190d649199f039e04
hehehai <riverhohai@gmail.com>
2024-02-18 11:10:44 +0800

    feat: new post

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,15 +12,15 @@ export default function Document() {
           crossOrigin="anonymous"
           href="https://cdn.jsdelivr.net/npm/misans@4.0.0/lib/Normal/MiSans-Regular.min.css"
         />
-        <Script id="ms_clarity" strategy="afterInteractive">
-          {`(function(c,l,a,r,i,t,y){
-              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-          })(window, document, "clarity", "script", "kxysqce1d4");`}
-        </Script>
       </Head>
-      <body className="min-h-screen bg-stone-50 text-foreground h-full dark:bg-gradient-to-br dark:from-slate-900 dark:to-background">
+      <Script id="ms_clarity" strategy="afterInteractive">
+        {`(function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "kxysqce1d4");`}
+      </Script>
+      <body className="text-foreground dark:to-background h-full min-h-screen bg-stone-50 dark:bg-gradient-to-br dark:from-slate-900">
         <Main />
         <NextScript />
       </body>
