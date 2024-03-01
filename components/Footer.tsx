2478528dd37bfd24e2b1fb3f9f27b29b28b51143
hehehai <riverhohai@gmail.com>
2024-03-01 10:12:51 +0800

    chore: ICP number

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,18 @@
 export default function Footer() {
   const currentYear = new Date().getFullYear();
 
-  return <div className="py-8 mt-20">© 2019 - {currentYear}, Hehehai</div>;
+  return (
+    <div className="py-8 mt-20 flex items-center">
+      <span>© 2019 - {currentYear}, Hehehai </span>
+      <a
+        href="http://beian.miit.gov.cn/"
+        target="_blank"
+        rel="noreferrer"
+        className="ml-2 text-foreground/90"
+        title="备案号"
+      >
+        晋ICP备2024032508号-1
+      </a>
+    </div>
+  );
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,5 @@
 import { cn } from "lib/utils";
 import { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script";
 
 export default function Document() {
   return (
@@ -13,13 +12,6 @@ export default function Document() {
           href="https://cdn.jsdelivr.net/npm/misans@4.0.0/lib/Normal/MiSans-Regular.min.css"
         />
       </Head>
-      <Script id="ms_clarity" strategy="afterInteractive">
-        {`(function(c,l,a,r,i,t,y){
-            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-        })(window, document, "clarity", "script", "kxysqce1d4");`}
-      </Script>
       <body className="text-foreground dark:to-background h-full min-h-screen bg-stone-50 dark:bg-gradient-to-br dark:from-slate-900">
         <Main />
         <NextScript />
