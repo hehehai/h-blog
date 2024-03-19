3e56d3062e7297873e2288106bbbe46d3344a1b0
hehehai <riverhohai@gmail.com>
2024-03-19 10:33:23 +0800

    fix: GA script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,24 +14,24 @@ export default function Document() {
           crossOrigin="anonymous"
           href="https://cdn.jsdelivr.net/npm/misans@4.0.0/lib/Normal/MiSans-Regular.min.css"
         />
-        <Script
-          src={`https://www.googletagmanager.com/gtag/js?id=${GoogleGA}`}
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', ${GoogleGA});
-        `}
-        </Script>
       </Head>
       <body className="text-foreground dark:to-background h-full min-h-screen bg-stone-50 dark:bg-gradient-to-br dark:from-slate-900">
         <Main />
         <NextScript />
       </body>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GoogleGA}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${GoogleGA}');
+        `}
+      </Script>
     </Html>
   );
 }
