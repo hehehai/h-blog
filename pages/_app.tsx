9870ec4727b1bf0e0f189279cc5e91046a9912b6
hehehai <riverhohai@gmail.com>
2024-04-04 19:39:39 +0800

    fix: img width

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,4 @@
 import "highlight.js/styles/atom-one-dark-reasonable.css";
-import "../styles/tailwind.css";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
