1b2f5604b15e818a756d5153c21cc4979e20d166
hehehai <riverhohai@gmail.com>
2024-04-27 12:48:34 +0800

    chore: ga with openpanel

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,8 +1,6 @@
 import { cn } from "lib/utils";
 import { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script";
-
-const GoogleGA = "G-S6MF4DWGZW";
+import { OpenpanelProvider } from "@openpanel/nextjs";
 
 export default function Document() {
   return (
@@ -14,24 +12,19 @@ export default function Document() {
           crossOrigin="anonymous"
           href="https://cdn.jsdelivr.net/npm/misans@4.0.0/lib/Normal/MiSans-Regular.min.css"
         />
-        <Script
-          src={`https://www.googletagmanager.com/gtag/js?id=${GoogleGA}`}
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', '${GoogleGA}');
-        `}
-        </Script>
       </Head>
       <body className="text-foreground dark:to-background h-full min-h-screen bg-stone-50 dark:bg-gradient-to-br dark:from-slate-900">
         <Main />
         <NextScript />
       </body>
+      {process.env.NODE_ENV === "production" && (
+        <OpenpanelProvider
+          clientId="1be734c9-2c9f-4451-ae69-745c89ca59cd"
+          trackScreenViews={true}
+          trackAttributes={true}
+          trackOutgoingLinks={true}
+        />
+      )}
     </Html>
   );
 }
