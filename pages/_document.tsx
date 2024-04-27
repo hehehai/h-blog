bb366b1ad379617499905b69e7e0c1f31cd09492
hehehai <riverhohai@gmail.com>
2024-04-27 22:49:15 +0800

    fix: ga env

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,18 +12,15 @@ export default function Document() {
           crossOrigin="anonymous"
           href="https://cdn.jsdelivr.net/npm/misans@4.0.0/lib/Normal/MiSans-Regular.min.css"
         />
-        {process.env.NODE_ENV === "production" && (
-          <>
-            {" "}
-            <Script
-              id="openpanel"
-              src="https://openpanel.dev/op.js"
-              defer
-              async
-              strategy="afterInteractive"
-            />
-            <Script id="openpanel-client" strategy="afterInteractive">
-              {`
+        <Script
+          id="openpanel"
+          src="https://openpanel.dev/op.js"
+          defer
+          async
+          strategy="afterInteractive"
+        />
+        <Script id="openpanel-client" strategy="afterInteractive">
+          {`
       window.op = window.op || function (...args) { (window.op.q = window.op.q || []).push(args); };
       window.op('ctor', {
         clientId: '1be734c9-2c9f-4451-ae69-745c89ca59cd',
@@ -31,9 +28,7 @@ export default function Document() {
         trackOutgoingLinks: true,
         trackAttributes: true,
       });`}
-            </Script>
-          </>
-        )}
+        </Script>
       </Head>
       <body className="text-foreground dark:to-background h-full min-h-screen bg-stone-50 dark:bg-gradient-to-br dark:from-slate-900">
         <Main />
