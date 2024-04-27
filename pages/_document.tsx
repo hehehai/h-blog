fbe940cedeb361191f2fbdc27b1db288db774892
hehehai <riverhohai@gmail.com>
2024-04-27 22:44:51 +0800

    fix: openpanel script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,6 @@
 import { cn } from "lib/utils";
 import { Html, Head, Main, NextScript } from "next/document";
-import { OpenpanelProvider } from "@openpanel/nextjs";
+import Script from "next/script";
 
 export default function Document() {
   return (
@@ -12,19 +12,33 @@ export default function Document() {
           crossOrigin="anonymous"
           href="https://cdn.jsdelivr.net/npm/misans@4.0.0/lib/Normal/MiSans-Regular.min.css"
         />
+        {process.env.NODE_ENV === "production" && (
+          <>
+            {" "}
+            <Script
+              id="openpanel"
+              src="https://openpanel.dev/op.js"
+              defer
+              async
+              strategy="afterInteractive"
+            />
+            <Script id="openpanel-client" strategy="afterInteractive">
+              {`
+      window.op = window.op || function (...args) { (window.op.q = window.op.q || []).push(args); };
+      window.op('ctor', {
+        clientId: '1be734c9-2c9f-4451-ae69-745c89ca59cd',
+        trackScreenViews: true,
+        trackOutgoingLinks: true,
+        trackAttributes: true,
+      });`}
+            </Script>
+          </>
+        )}
       </Head>
       <body className="text-foreground dark:to-background h-full min-h-screen bg-stone-50 dark:bg-gradient-to-br dark:from-slate-900">
         <Main />
         <NextScript />
       </body>
-      {process.env.NODE_ENV === "production" && (
-        <OpenpanelProvider
-          clientId="1be734c9-2c9f-4451-ae69-745c89ca59cd"
-          trackScreenViews={true}
-          trackAttributes={true}
-          trackOutgoingLinks={true}
-        />
-      )}
     </Html>
   );
 }
